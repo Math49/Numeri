622ad4ef197aa2365e26f97c8c1d89aa391a2796
Hockwood <45720517+Math49@users.noreply.github.com>
2023-04-07 16:09:09 +0200

    Update head.php

--- Fragments/head.php
+++ Fragments/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="Style/header.css">
     <link rel="stylesheet" href="Style/footer.css">
-    <link rel="icon" href="Materiels\Logo\Logo_Numéri_Coloré.png"
+    <link rel="icon" href="Materiels\Logo\Logo_Numéri_Coloré.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
